c040d32935b54430cb7560feae86d76e88edced6
jfhaecker <jfhaeckergmail.com>
2019-11-14 19:05:06 +0100

    first commit works

--- main.go
+++ main.go
@@ -6,40 +6,131 @@ import (
 	"io/ioutil"
 )
 
-type Blob struct {
-	content   []byte
-	ofilename string
-	nfilename string
-	hashCode  []byte
+var (
+	newline = "\n"
+	//index   = make([]GitObject, 0)
+	index = make(map[string]GitObject)
+)
+
+type GitObject interface {
+	Type() []byte
+	DoHash()
+	GetHashCode() []byte
+	Store()
+	GetOFilename() string
+	CreateFileContent()
 }
 
 type Tree struct {
 	ofilename string
 	nfilename string
-	blobs     []Blob
-	subtrees  []Tree
+	children  []GitObject
+	content   []byte
+	hashCode  []byte
 }
 
-func (b *Blob) blob() {
+func (t *Tree) Store() {
+	ioutil.WriteFile(t.nfilename, t.content, 0644)
+}
+
+func (t *Tree) DoHash() {
 	h := sha256.New()
-	h.Write([]byte(b.content))
-	b.hashCode = h.Sum(nil)
-	b.content = append([]byte("blob\n"), b.content...)
+	for _, child := range t.children {
+		h.Write(child.GetHashCode())
+	}
+	t.hashCode = h.Sum(nil)
+	t.nfilename = fmt.Sprintf("%x", t.hashCode)
+
+}
+
+func (t Tree) Type() []byte {
+	return []byte("tree")
+}
+
+type Blob struct {
+	content     []byte
+	ofilename   string
+	nfilename   string
+	hashCode    []byte
+	filecontent []byte
+}
+
+func (b *Blob) CreateFileContent() {
+	b.filecontent = []byte{}
+	header := []byte{}
+	header = append(b.Type(), []byte(newline)...)
+	b.filecontent = append(header, b.content...)
 	b.nfilename = fmt.Sprintf("%x", b.hashCode)
 }
 
-func main() {
-	hashObject([]byte("hallo"), "1.txt")
-	hashObject([]byte("tolles"), "2.txt")
-	hashObject([]byte("programm"), "3.txt")
+func (b Blob) Type() []byte {
+	return []byte("blob")
 }
 
-func hashObject(content []byte, filename string) {
+func (b *Blob) DoHash() {
+	h := sha256.New()
+	h.Write([]byte(b.content))
+	b.hashCode = h.Sum(nil)
+}
 
+func (b *Blob) GetOFilename() string {
+	return b.ofilename
+}
+
+func (b *Blob) GetHashCode() []byte {
+	return b.hashCode
+}
+
+func (b *Blob) Store() {
+	//fmt.Printf("---> %v Storing: [%v]\n", b.nfilename, string(b.content))
+	ioutil.WriteFile(b.nfilename, b.filecontent, 0644)
+
+}
+
+func main() {
+	add([]byte("hallo1"), "1.txt")
+	add([]byte("hola1"), "2.txt")
+	add([]byte("servus1"), "3.txt")
+	fmt.Printf("\nIndex: %v\n", len(index))
+	commit()
+	add([]byte("servus2"), "3.txt")
+	fmt.Printf("\nIndex: %v\n", len(index))
+	commit()
+}
+
+func add(content []byte, filename string) {
 	b := Blob{content: content, ofilename: filename}
-	b.blob()
+	index[filename] = &b
+	//index = append(index, &b)
+	//fmt.Printf("%v\n", b.nfilename)
+}
 
-	fmt.Printf("%v\n", b.nfilename)
-	ioutil.WriteFile(b.nfilename, b.content, 0644)
+func commit() {
+	fmt.Println("===========Commit")
+	tree := &Tree{}
+	tree.children = make([]GitObject, 0)
+	for _, obj := range index {
+		obj.DoHash()
+		//fmt.Printf("Hallo:%v\n", obj)
+		obj.Store()
+		tree.children = append(tree.children, obj)
+		//hc := fmt.Sprintf("%x", obj.GetHashCode)
+		//fmt.Printf("%v:%x\n", string(obj.Type()), obj.GetHashCode())
+	}
+	header := []byte{}
+	header = append(tree.Type(), []byte(newline)...)
+	tree.content = append(tree.content, header...)
+	for _, child := range tree.children {
+		c := fmt.Sprintf("%v %x %v \n",
+			string(child.Type()),
+			child.GetHashCode(),
+			child.GetOFilename())
+		tree.content = append(tree.content, []byte(c)...)
+		//fmt.Printf("%v", c)
 
+	}
+	tree.DoHash()
+	fmt.Printf("Tree : %x\n", string(tree.hashCode))
+	fmt.Printf("%v\n", string(tree.content))
+	tree.Store()
 }
